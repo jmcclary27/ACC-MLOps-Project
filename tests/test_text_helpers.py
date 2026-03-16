d36755516b728d67dbbc4d4cac2a28f6dd8e08af
Jaden <jadenmcclary@gmail.com>
2026-03-16 16:37:02 -0500

    test fixes

--- tests/test_text_helpers.py
+++ tests/test_text_helpers.py
@@ -10,7 +10,7 @@ from ml.data.text_helpers import pdf_to_text, split_into_sentence, clean_text
 def test_pdf_to_text():
     
     # test with a sample pdf file
-    text = pdf_to_text("tests/Test_files/sample.pdf", as_text=True)
+    text = pdf_to_text("tests/sample.pdf", as_text=True)
     #check for string type, non empty, correct content
     assert isinstance(text, str)
     assert len(text) > 0
@@ -18,12 +18,12 @@ def test_pdf_to_text():
     #checking for the new line characters
     print(repr(text))
 def test_split_into_sentence():
-    text = pdf_to_text("tests/Test_files/sample.pdf", as_text=True)
+    text = pdf_to_text("tests/sample.pdf", as_text=True)
     sentences = split_into_sentence(text)
     result = ["Hello, this is a test PDF.", "This agreement shall terminate upon breach.", "The tenant must pay a rent of $500 on the first of each month. \n\n"]
     assert result == sentences
 def test_clean_text():
-    text = pdf_to_text("tests/Test_files/sample.pdf", as_text=True)
+    text = pdf_to_text("tests/sample.pdf", as_text=True)
     cleaned_text = clean_text(text)
     assert cleaned_text == "Hello, this is a test PDF. This agreement shall terminate upon breach. The tenant must pay a rent of $500 on the first of each month."
 
